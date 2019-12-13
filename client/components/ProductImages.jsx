f734fa8ea6af7090ceb9011af9b2b458eb35c8a0
David Clay <dclay1983@gmail.com>
2019-12-13 09:58:31 -0700

    Implement seed server and basic frontend

---
+++ client/components/ProductImages.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import MainImage from './MainImage.jsx';
+import ImageList from './ImageList.jsx';
+
+class ProductImages extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: 0
+    };
+  }
+
+  render() {
+    return (
+      <div className='imageModule'>
+        <ImageList selected={this.state.selected} images={this.props.images}/>
+        <MainImage selected={this.state.selected} image={this.props.images[this.state.selected]}/>
+      </div>
+    )
+  }
+}
+
+export default ProductImages;
